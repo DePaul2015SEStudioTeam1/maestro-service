5d301848dc959f7d3823b4f8b92cc4facb42a109
John Plante <johnrplante@gmail.com>
2014-11-16 17:07:02 -0600

    committing to push changes for MaestroServiceImpl

---
+++ src/test/java/edu/depaul/maestro/ServiceIntegrationTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.depaul.maestro;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import edu.depaul.maestro.service.MaestroService;
+import edu.depaul.operations.model.Container;
+
+/**
+ * @author ptrzyna
+ */
+public class ServiceIntegrationTest {
+
+	public static void main(String[] args) throws Exception {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans/maestro-client.xml");
+		MaestroService<Container> service = context.getBean("remoteMaestroService", MaestroService.class);
+		
+		//Agent agent = new Agent();
+		
+		//while(true) {
+		//	Container container = agent.getContainer();
+		//	service.store(container);
+		//	Thread.sleep(3000);
+		//}
+	}
+	
+}
