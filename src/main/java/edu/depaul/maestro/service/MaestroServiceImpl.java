e5960a5139cf13d42d4af3dd89e4ac3580aa6e6a
John Plante <johnrplante@gmail.com>
2014-11-17 18:30:41 -0600

    test

--- src/main/java/edu/depaul/maestro/service/MaestroServiceImpl.java
+++ src/main/java/edu/depaul/maestro/service/MaestroServiceImpl.java
@@ -5,15 +5,17 @@ package edu.depaul.maestro.service;
 
 import edu.depaul.operations.model.Container;
 import edu.depaul.operations.service.OperationsService;
+import org.apache.log4j.Logger;
 
 /**
  * Concrete implementation of the OperationsService
  * 
- * @author Paul A. Trzyna
+ * @author John Plante
  */
 public class MaestroServiceImpl implements MaestroService<Container> {
     
     private OperationsService<Container> operationsService;
+    final Logger logger = Logger.getLogger(MaestroServiceImpl.class);
     
     public void setOperationsService(OperationsService<Container> operationsService){
         this.operationsService = operationsService;
@@ -23,7 +25,16 @@ public class MaestroServiceImpl implements MaestroService<Container> {
 	 * @see edu.depaul.service.OperationsService#store(edu.depaul.model.Container)
 	 */
 	public void store(Container container) {
-        operationsService.store(container);
+        
+		if(logger.isDebugEnabled()){
+			logger.debug("Container received by Maestro. Container ID: " + container.getId() +
+					" Sent by Agent ID: " + container.getAgentId());
+		}
+		
+		logger.error("There was a problem with the container received.", new Exception("Testing"));
+		
+		operationsService.store(container);        
+        
 	}
 
 }
